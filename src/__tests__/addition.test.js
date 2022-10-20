74900eb7bb258553ad3d24dce5e15f1efe7bdf6c
Ali Fadhel <alifadhel619@gmail.com>
2022-10-20 16:13:01 +0200

    second commit

---
+++ src/__tests__/addition.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+test("it shoud return the addition of a and b",()=>{
+
+    expect(5+7).toBe(12);
+    expect(10-3).toEqual(7);
+    expect(5*5).toEqual(25);
+    expect(10%2).toEqual(0);
+});
